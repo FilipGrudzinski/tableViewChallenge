1d85118809a49bcc6e739c815afa791a62ea5cb7
FilipGrudzinski <grudzinski.filip@gmail.com>
2018-04-17 20:33:32 +0200

    tableView with loop

--- tableViewChallenge/ViewController.swift
+++ tableViewChallenge/ViewController.swift
@@ -8,8 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+ 
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        var i = 1
+        
+        while i < 50 {
+            
+            i += 1
+            
+        }
+        
+        return i
+        
+    }
+    
+    
+   
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+      let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+    
+      cell.textLabel?.text = String(indexPath.row + 1)
+     
+      return cell
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
